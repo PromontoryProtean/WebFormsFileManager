bcc42909e068936e0d1d0731c4350ee2104f7e5d
John Saccoccio <john@cybertechnical.com>
2016-01-04 16:06:25 -0500

    Web.config changes to set upload size limits to more practical numbers and add tag mapping for JqueryRemovedScriptManager.

---
+++ Web/App_Code/JqueryRemovedScriptManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+
+public class JqueryRemovedScriptManager : ScriptManager
+{
+    protected override void OnInit(EventArgs e)
+    {
+        Page.PreRenderComplete += Page_PreRenderComplete;
+        base.OnInit(e);
+    }
+
+    private void Page_PreRenderComplete(object sender, EventArgs e)
+    {
+        var jqueryReferences = Scripts.Where(s => s.Name.Equals("jquery", StringComparison.OrdinalIgnoreCase)).ToList();
+        if (jqueryReferences.Count > 0)
+        {
+            // Remove the jquery references as we're rendering it manually in the master page <head>
+            foreach (var reference in jqueryReferences)
+            {
+                Scripts.Remove(reference);
+            }
+        }
+    }
+}
